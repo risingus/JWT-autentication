ebe0094965453641db38a8ceea740dc6555a362d
Gustavo <gustavo.it@outlook.com>
2021-08-16 14:34:50 -0300

    Test
    Test of jwt autentication

---
+++ services/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import { parseCookies } from 'nookies';
+
+const cookies = parseCookies();
+
+console.log(cookies)
+
+export const api = axios.create({
+  baseURL: 'http://localhost:3333',
+  headers: {
+    Authorization: `Bearer ${cookies['nextauth.token']}`
+  }
+})
